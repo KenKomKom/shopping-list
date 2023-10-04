665b2885ff96768524cb94020af35b304f9281b7
KenKomKom <119410845+KenKomKom@users.noreply.github.com>
2023-10-04 13:58:40 +0700

    UPDATE TUTORIAL5

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from main.views import  show_main, create_product, show_xml, \
                         show_json, show_xml_by_id, show_json_by_id, \
                         register, login_user, logout_user, edit_product,\
-                        delete_product
+                        delete_product, get_product_json, add_product_ajax\
+                        
 
 
 app_name = 'main'
@@ -18,5 +19,7 @@ urlpatterns = [
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<int:id>/', show_json_by_id, name='show_json_by_id'), 
     path('edit-product/<int:id>', edit_product, name='edit_product'),
-    path('delete/<int:id>', delete_product, name='delete_product')
+    path('delete/<int:id>', delete_product, name='delete_product'),
+    path('get-product/', get_product_json, name='get_product_json'),
+    path('create-product-ajax/', add_product_ajax, name='add_product_ajax'),
 ]

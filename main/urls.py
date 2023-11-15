89c88e0b6883ac2e3520eacc7a732b6763e74e33
KenKomKom <119410845+KenKomKom@users.noreply.github.com>
2023-11-15 13:46:00 +0700

    a

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,7 @@ from main.views import  show_main, create_product, show_xml, \
                         show_json, show_xml_by_id, show_json_by_id, \
                         register, login_user, logout_user, edit_product,\
                         delete_product, get_product_json, add_product_ajax,\
-                        tes
+                        tes, create_product_flutter
                         
 
 
@@ -24,4 +24,5 @@ urlpatterns = [
     path('delete/<int:id>', delete_product, name='delete_product'),
     path('get-product/', get_product_json, name='get_product_json'),
     path('create-product-ajax/', add_product_ajax, name='add_product_ajax'),
+    path('create-flutter/', create_product_flutter, name='create_product_flutter'),
 ]
